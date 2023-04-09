cc1afd5b989bfecd25d2c32f7bc97eedca97faaf
Jonah Warren <jonahewarren@gmail.com>
2023-04-08 21:23:45 -0500

    initial pacman commit

---
+++ src/Tilemap.js
@@ -0,0 +1,26 @@
+export default class TileMap{
+    constructor(tileSize) {
+        this.tileSize = tileSize;
+
+        this.yellowdot = new Image()
+    }   this.yellowdot.src = "../images/yellowdot.png"
+
+        this.wall = new Image()
+        this.wall.src = "../images/wall.png";
+    map = [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    ];
+
+    draw() {
+   // console.log("draw")
+  }
+}
---
+++ src/game.js
@@ -0,0 +1,13 @@
+import TileMap from './Tilemap.js';
+
+const tileSize = 32;
+const canvas = document.getElementById('gameCanvas')
+const ctx = canvas.getContext('2d')
+const tileMap = new TileMap(tileSize);
+
+function gameLoop() {
+    tileMap.draw();
+console.log("game loop")
+}
+
+setInterval(gameLoop, 1000 / 75);
